1e9130802b45faf703e8ff2f9fbde5b77962b50d
Maanit972 <maanitmalhan@gmail.com>
2024-06-14 13:47:11 -0400

    styling changed to meet uconn standards

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,13 @@
-import { Link } from 'react-router-dom';
+import uconn_white from '../Assets/uconn_white.png';
 
 const Navbar = () => {
     return (
         <header className="header">
-        <a href="https://iac.university/" className='header-img-link'>
-        <img src="https://www.energy.gov/sites/default/files/styles/full_article_width/public/2018/05/f51/Industrial%20Assessment%20Center%20-%20Logo.png?itok=dNqt_m33"
-        alt="Industrial Assessment Center Logo" className='header-img'/>
+        <a href="https://uconn.edu/" className='header-img-link'>
+        <img src={uconn_white}
+        alt="UConn Logo" className='header-img'/>
         </a>
         <h1>Southern New England Industrial Assessment Center Internal Tool</h1>
-        <hr className="line" />
     </header>
     )};
 
