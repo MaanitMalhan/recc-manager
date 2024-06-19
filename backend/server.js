508f7a4bc418788c232a1134422dc41171b26197
Maanit972 <maanitmalhan@gmail.com>
2024-06-19 17:48:05 -0400

    docker half working issues with fronend/backend connection

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const reccRoutes = require('./routes/Routes');
+const reccRoutes = require('./routes/routes');
 
 //creates express app
 const app = express();
