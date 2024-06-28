00948e8887ed5a5a3853632e93c63a244d27de24
Maanit972 <maanitmalhan@gmail.com>
2024-06-28 10:27:33 -0400

    sign up logic and validation for email/passwords added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +15,36 @@ const userSchema = new Schema({
     required: true
   }
 })
+
+//Static sign up method
+userSchema.statics.signup = async function(email, password) {
+
+  //validation
+  if (!email || !password) {
+    throw Error('Email and password are required')
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error('Email is invalid')
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough')
+  }
+
+  const exists = await this.findOne({ email })
+
+  if (exists) {
+    throw new Error('Email already exists')
+  }
+  //hash the password
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+
+  const user = await this.create({ email, password: hash })
+
+  return user;
+
+}
 
 module.exports = mongoose.model('User', userSchema)
