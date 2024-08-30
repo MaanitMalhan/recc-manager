d36e1f93e1273ad4ef684ddb5aeac3d1e3b72457
Maanit972 <maanitmalhan@gmail.com>
2024-08-30 17:19:07 -0400

    error fix for docker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { useAuthContext } from './hooks/useAuthContext'
 // pages & components
 import Home from './pages/Home'
 import Login from './pages/Login'
-import Signup from './pages/Signup'
+//import Signup from './pages/Signup'
 import Navbar from './components/Navbar'
 import CSVpage from './pages/CSVpage'
 
